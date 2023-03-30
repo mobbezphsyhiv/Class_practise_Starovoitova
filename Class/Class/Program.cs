f7ecb5dda462d7ac593a685d7df54fbde4033343
mobbezphsyhiv <127360324+mobbezphsyhiv@users.noreply.github.com>
2023-03-30 14:33:29 +0300

    Add files via upload

---
+++ Class/Class/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+   
+    class Mage 
+    {
+        protected int healthbar = 100;
+        protected int enemy_damage = 20;
+        private int all_attack_damage;
+
+        public void Attack()
+        {
+            
+            if (healthbar<=0)
+            {
+                Console.WriteLine("Your Mage is Dead");
+                Console.WriteLine();
+            }
+            else
+            {
+                healthbar -= 10;
+                all_attack_damage = all_attack_damage+enemy_damage;
+                Console.WriteLine($"Mage's health: {healthbar}\n Mage's attack damage:{all_attack_damage} ");
+                Console.WriteLine();
+            }
+
+            
+        }
+
+    }
+
+    class LegendaryMage : Mage
+    {
+        private int healing_capasity = 5;
+
+        public void Healing()
+        {
+            if (healthbar==100 || healthbar ==0)
+            {
+                Console.WriteLine("Unable to heal");
+            }
+            else
+            {
+                healthbar = healthbar + healing_capasity;
+                enemy_damage--;
+                Console.WriteLine($"Mage's health after healing: {healthbar} ");
+                Console.WriteLine($"Mage's current enemy damage capasity: {enemy_damage} ");
+            }
+            
+        }
+    }
+
+    class Knights
+    {
+        private int attack_capacity = 5;
+        private int all_attack_damage;
+        private int army_number;
+        public Knights(int n)
+        {
+            army_number = n;
+        }
+        
+       
+        private int Reinforcement()
+        {
+            attack_capacity++;
+            return attack_capacity;
+        }
+
+        public void Attack()
+        {          
+
+            if (army_number<=0)
+            {
+                Console.WriteLine("All your knights are dead");
+                Console.WriteLine();
+            }
+            else
+            {
+
+                Reinforcement();
+                all_attack_damage = all_attack_damage + attack_capacity * army_number;
+                Console.WriteLine($"Current army number: {army_number}\n All attack damage {all_attack_damage}");
+                Console.WriteLine();
+                army_number--;
+            }
+            
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mage first = new Mage();
+
+            for (int i = 0; i < 20; i++)
+            {
+                first.Attack();
+            }
+
+            LegendaryMage first1 = new LegendaryMage();
+
+            first1.Healing();
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                first1.Attack();
+            }
+            first1.Healing();
+
+            LegendaryMage second2 = new LegendaryMage();
+
+            for (int i = 0; i < 15; i++)
+            {
+                first.Attack();
+            }
+
+            second2.Healing();
+
+            Console.Write("Input the number of knights: ");
+
+            int knights_num = int.Parse(Console.ReadLine());
+            Knights first3 = new Knights(knights_num);
+
+            for (int i = 0; i < 15; i++)
+            {
+                first3.Attack();
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Class/Class_Pattern/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Pattern
+{
+    class Program
+    {
+
+        class Fifo
+        {
+
+            string[] data;
+            public void add(string s)
+            {
+                ///
+            }
+
+            public string get()
+            {
+                return "";
+            }
+
+        }
+
+
+        abstract class Stationery
+        {
+
+
+        }
+
+        abstract class ToolstoDraw : Stationery
+        {
+            protected string color;
+            protected int thickness;
+            protected int price;
+
+            public abstract void draw();
+
+            protected void doSomething()
+            {
+                Console.WriteLine("doSomething");
+            }
+
+        }
+
+        class Pencil : ToolstoDraw
+        {
+            private bool isHaveEraser;
+
+            public override void draw()
+            {
+                Console.WriteLine("Pencil is drawing");
+            }
+
+        }
+
+        class Pen : ToolstoDraw
+        {
+            private int pasta = 100;
+            private int ink_type;
+            private bool isAuto;
+
+            static private int globalCount = 0;
+
+            static public void Something()
+            {
+                Console.WriteLine("There are " + globalCount + " pens");
+            }
+
+            public override void draw()
+            {
+                base.doSomething();
+                //base.doDo();
+                if (pasta > 0)
+                {
+                    Console.WriteLine("Pen is drawing");
+                    pasta = pasta - 1;
+                }
+                else
+                {
+
+                }
+            }
+
+            public int getPasta()
+            {
+                return pasta;
+            }
+
+            public Pen(int pasta, int ink_type, bool isAuto,
+                string color, int thickness, int price)
+            {
+                this.color = color;
+                this.thickness = thickness;
+                this.price = price;
+                this.pasta = pasta;
+                this.ink_type = ink_type;
+                this.isAuto = isAuto;
+                globalCount++;
+            }
+
+            public Pen(int pasta, int ink_type, bool isAuto)
+            {
+                this.pasta = pasta;
+                this.ink_type = ink_type;
+                this.isAuto = isAuto;
+                globalCount++;
+            }
+
+            public Pen()
+            {
+                globalCount++;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Pen p1 = new Pen(100, 2, true);
+            Pen p2 = new Pen(100, 1, false);
+            Pen p3 = new Pen(100, 1, false, "red", 3, 100);
+            Pencil pencil = new Pencil();
+
+            Pen p4 = new Pen();
+            p4.draw();
+
+            //Pen.Something();
+
+            //Console.WriteLine(p4.getPasta());
+            //p4.draw();
+            //p4.draw();
+            //p4.draw();
+            //p4.draw();
+            //Console.WriteLine(p4.getPasta());
+            //
+
+
+
+            //ToolstoDraw[] arrOfTools = new ToolstoDraw[2];
+            //arrOfTools[0] = p1;
+            //arrOfTools[1] = pencil;
+
+            //foreach (ToolstoDraw element in arrOfTools) {
+            //    element.draw();
+
+            //}
+
+
+        }
+    }
+}
